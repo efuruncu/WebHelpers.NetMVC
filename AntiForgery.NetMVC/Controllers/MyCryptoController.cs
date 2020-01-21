31e3b58db4a3fab51c2977c71b422c8dbc4052e5
Elif Furuncu <eliffuruncu93@gmail.com>
2020-01-21 14:26:10 +0300

    Crypto and WebImage added.

---
+++ AntiForgery.NetMVC/Controllers/MyCryptoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace AntiForgery.NetMVC.Controllers
+{
+    public class MyCryptoController : Controller
+    {
+        private string sifresiz = "Elif Furuncu";
+        private string sifreli = "htHenv17a2TiZ29F38LJ+x/kOjfxtke/eU7ucHn9twc=";
+        // GET: MyCrypto
+        public ActionResult Index()
+        {
+            //Random anahtar değer verir
+            string salt = Crypto.GenerateSalt();
+
+            //Şifreleme
+            string hash = Crypto.Hash(sifresiz,algorithm:"md5");
+            string sh1 = Crypto.SHA1(sifresiz);
+            string sh256 = Crypto.SHA256(sifresiz);
+
+            //şifresiz bir metni şifreliyor
+            string sonuc = Crypto.HashPassword(sifresiz);
+            //şifreli bir metni şifresiz ile karşılaştırıp doğru mu olduğuna bakıyor.
+            bool dogrumu = Crypto.VerifyHashedPassword(sifreli, sifresiz);
+
+            return View();
+
+            
+        }
+    }
+}
---
+++ AntiForgery.NetMVC/Controllers/MyWebImageController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace AntiForgery.NetMVC.Controllers
+{
+    public class MyWebImageController : Controller
+    {
+        // GET: MyWebImage
+
+        public string ImagePath
+        {
+            get { return Server.MapPath("~/images/cat.jpg"); }
+        }
+        public ActionResult Start(string cmd="Original")
+        {
+            ViewBag.cmd = cmd;
+            return View();
+        }
+
+        public void Process(string p)
+        {
+            WebImage image = new WebImage(this.ImagePath);
+
+            switch (p)
+            {
+                case "Original":
+                    break;
+                case "RotateLeft":
+                    image.RotateLeft();
+                    break;
+                case "RotateRight":
+                    image.RotateRight();
+                    break;
+                case "FlipHorizontal":
+                    image.FlipHorizontal();
+                    break;
+                case "FlipVertical":
+                    image.FlipVertical();
+                    break;
+                case "Resize":
+                    image.Resize(image.Width / 2,image.Height /2 ,preserveAspectRatio:true);
+                    break;
+                case "AddTextWatermark":
+                    image.AddTextWatermark("Elif Furuncu",fontColor:"Red",fontSize:14,horizontalAlign:"Center",verticalAlign:"Bottom");
+                    break;
+                case "AddImageWatermark":
+                    WebImage watermark = new WebImage(this.ImagePath);
+                    watermark.Resize(50,50);
+                    watermark = watermark.Save(Server.MapPath("~/images/watermark.jpeg"),imageFormat:"jpeg");
+                    image.AddImageWatermark(watermark.FileName, 50, 50, verticalAlign: "Top",horizontalAlign:"Right",opacity:75);
+                    break;
+                default:
+                    break;
+            }
+            image.Write();
+        }
+    }
+}
