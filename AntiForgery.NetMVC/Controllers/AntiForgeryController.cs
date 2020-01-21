3423dd81a2e7b82075edb41a0dc01964e05e040d
Elif Furuncu <eliffuruncu93@gmail.com>
2020-01-21 10:27:36 +0300

    first commit

---
+++ AntiForgery.NetMVC/Controllers/AntiForgeryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AntiForgery.NetMVC.Models;
+
+namespace AntiForgery.NetMVC.Controllers
+{
+    public class AntiForgeryController : Controller
+    {
+        // GET: AntiForgery
+        public ActionResult Test()
+        {
+            return View(Veritabani.Veriler);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Test(int id)
+        {
+            Veritabani.Veriler.RemoveAt(id);
+            return RedirectToAction("Test");
+        }
+
+        public ActionResult FakeTest()
+        {
+            return View();
+        }
+    }
+}
---
+++ AntiForgery.NetMVC/Models/Veritabani.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AntiForgery.NetMVC.Models
+{
+    public class Veritabani
+    {
+        public static List<string> Veriler = new List<string>(
+     new string[] { "Elif", "Yunus", "Emir", "Esra", "Yasemin" });
+
+    }
+}
