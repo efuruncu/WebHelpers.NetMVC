a49c46dcf49f4784d79ea3779aa7a0b6c5b803bd
Elif Furuncu <eliffuruncu93@gmail.com>
2020-01-21 15:10:03 +0300

    WebCache added.

---
+++ AntiForgery.NetMVC/Controllers/MyWebCacheController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace AntiForgery.NetMVC.Controllers
+{
+    public class MyWebCacheController : Controller
+    {
+        //Veritabanından çektiğimiz, değişmeyen kategori 
+        //yada verileri cache de tutarak zamandan tasarruf edebiliriz.
+        // GET: MyWebCache
+        public ActionResult Index()
+        {
+            string time = WebCache.Get("zaman");
+            if (string.IsNullOrEmpty(time))
+            {
+                time = DateTime.Now.ToString();
+                WebCache.Set(key: "zaman", value: time, minutesToCache: 1, slidingExpiration: true);
+            }
+            ViewBag.Simdi = time;
+
+            return View();
+        }
+
+        public ActionResult RemoveCache()
+        {
+            WebCache.Remove("zaman");
+            return RedirectToAction("Index");
+        }
+    }
+}
